a56e372170a793b5073dd41b6ee2e6ea82a07c81
= <=>
2026-03-11 16:59:19 +0700

    Adding tag-setter
    Added features: Using gemini-api to set tag for news

--- src/main.py
+++ src/main.py
@@ -1,7 +1,21 @@
+from google.genai import types
+import os
+from dotenv import load_dotenv
+from google import genai
 import json
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+result_schema = {
+    "name": "string",
+    "publishers": "string",
+    "time": "string",
+    "authors": "string",
+    "tags": "array",
+}
 
 
 def has_aria_label_and_href(tag):
@@ -33,6 +47,24 @@ def main():
             all_articles.append(article)
     with open("articles.json", "w") as fp:
         json.dump(all_articles, fp, indent=4, ensure_ascii=False)
+    with open("articles.json", "r") as fp:
+        json_cont = fp.read()
+    prompt = (
+        "Set tag for my news info json file from their title and context. Don't change any existing info. Remember to add indent in the output at each key = tag"
+        + "Here are the json file content: "
+        + json_cont
+    )
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt,
+        config=types.GenerateContentConfig(
+            response_json_schema=result_schema, response_mime_type="application/json"
+        ),
+    )
+    json_output = response.text
+    with open("articles.json", "w") as fp:
+        fp.write(json_output)
 
 
 if __name__ == "__main__":
